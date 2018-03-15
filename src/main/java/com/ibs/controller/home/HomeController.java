c90e535fb4e32305656e1c6cf0d2beea7b5320e1
Shalika Weerasinghe <udayangashalika92@gmail.com>
2018-03-15 14:38:03 +0530

    HomeController.java class initial commit

---
+++ src/main/java/com/ibs/controller/home/HomeController.java
@@ -0,0 +1,14 @@
+package com.ibs.controller.home;
+
+public class HomeController {
+	/**
+	 * setUserDetails()
+	 */
+	public void setUserDetails(){
+		try{
+			//TODO
+		}catch(Exception e){
+			System.out.println("Exception  :"+e);
+		}
+	}
+}
