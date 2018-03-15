62cad1a744f7733b2ec0c5dc000980ba221b9ffc
Shalika Weerasinghe <udayangashalika92@gmail.com>
2018-03-15 16:15:59 +0530

    *** HomeController.java class change ***

--- src/main/java/com/ibs/controller/home/HomeController.java
+++ src/main/java/com/ibs/controller/home/HomeController.java
@@ -1,11 +1,24 @@
 package com.ibs.controller.home;
 
 public class HomeController {
+	
 	/**
 	 * setUserDetails()
 	 */
 	public void setUserDetails(){
 		try{
+			//TODO
+		}catch(Exception e){
+			System.out.println("Exception1  :"+e);
+		}
+	}
+	
+	/**
+	 * setUserFavourites()
+	 */
+	public void setUserFavourites(){
+		try{
+			System.out.println("Exception  :");
 			//TODO
 		}catch(Exception e){
 			System.out.println("Exception  :"+e);
